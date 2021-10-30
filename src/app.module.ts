e4376cbf75492f7188ef696a825d551e72d6c4e0
Dionathan Cordova <dionathan.cordova@gmail.com>
2021-10-30 12:44:40 -0300

    feat: config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,31 +1,26 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
+import { AppConfigModule } from './config/api/config.module';
+import { DatabaseProviderModule } from './providers/database/provider.module';
+import { ModelsModule } from './modules/module.modules';
+import { APP_FILTER } from '@nestjs/core';
+import { QueryFailedExceptionFilter } from './config/filters/query-failed-exception-filter';
+import { EntityNotFoundExceptionFilter } from './config/filters/entity-not-found.exception-filter';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
-import { UsersController } from './controllers/users/users.controller';
-import { Usuarios } from './models/usuarios.model';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot(),
-
-    TypeOrmModule.forRoot({
-      // @ts-ignore
-      type: process.env.DB_CONNECTION,
-      host: process.env.DB_HOST,
-      port: Number(`${process.env.DB_PORT}`),
-      username: `${process.env.DB_USERNAME}`,
-      password: `${process.env.DB_PASSWORD}`,
-      database: `${process.env.DB_DATABASE}`,
-      migrations: ['project2/migrations/*.ts'],
-      cli: { migrationsDir: 'project2/migrations' },
-      entities: [Usuarios],
-      synchronize: true,
-    }),
-    TypeOrmModule.forFeature([Usuarios]),
+  imports: [AppConfigModule, DatabaseProviderModule, ModelsModule],
+  controllers: [AppController],
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: QueryFailedExceptionFilter,
+    },
+    {
+      provide: APP_FILTER,
+      useClass: EntityNotFoundExceptionFilter,
+    },
+    AppService,
   ],
-  controllers: [AppController, UsersController],
-  providers: [AppService],
 })
 export class AppModule {}
